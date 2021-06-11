b8060e95b3787918b604ceacb7cc4707e9d2fa42
Mangesh Panchal <Mangesh Panchal@DESKTOP-S1ENS9N>
2021-06-11 18:46:37 +0530

    update

---
+++ jQuery Weather API/jQuery Weather API/js/weather_api.js
@@ -0,0 +1,25 @@
+﻿function ajaxCall(data)
+{
+    var result;
+    $.ajax({
+        url: getURL(data),
+        type: 'POST',
+        data: [],
+        async: false,
+        success: function (data) {
+            result = data;
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            result = jqXHR.responseJSON.message;
+        }
+    });
+    return result;
+}
+
+function getWeatherByCityName(cityName) {
+    return ajaxCall(cityName);
+};
+
+function getURL(city) {
+    return "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=7312787cc87e04d375dc4906a2c8a25e";
+}
